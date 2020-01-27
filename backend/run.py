5399cc1c42d0337752acdd21bf69b30cb242aba2
Atanasov,Borislav B.S <b.atanasov@student.fontys.nl>
2020-01-27 10:23:59 +0100

    MTTQ and SoketIO connection added

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,10 @@
 # /run.py
 import os
+import eventlet
+import json
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO, emit
+from flask_bootstrap import Bootstrap
 from dotenv import load_dotenv, find_dotenv
 
 from src.app import create_app
@@ -9,7 +14,45 @@ load_dotenv(find_dotenv())
 env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
 
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+bootstrap = Bootstrap(app)
+
+
+@socketio.on('publish', namespace='/events')
+def handle_publish(json_str):
+    data = json.loads(json_str)
+    mqtt.publish(data['topic'], data['message'], data['qos'])
+
+
+@socketio.on('subscribe', namespace='/events')
+def handle_subscribe(json_str):
+    data = json.loads(json_str)
+    mqtt.subscribe(data['topic'], data['qos'])
+
+
+@socketio.on('unsubscribe_all', namespace='/events')
+def handle_unsubscribe_all():
+    mqtt.unsubscribe_all()
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode(),
+        qos=message.qos,
+    )
+    socketio.emit('test', data=data, namespace='/events')
+
+
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    # print(level, buf)
+    pass
+
+
 if __name__ == '__main__':
-  port = os.getenv('PORT')
-  # run app
-  app.run(host='localhost', port=port)
+    port = os.getenv('PORT')
+    # run app
+    socketio.run(app, port=port, use_reloader=False, debug=True)
--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,6 @@
 # src/app.py
 
-from flask import Flask
-
+from flask import Flask, render_template
 from .config import app_config
 from .models import db, bcrypt
 
@@ -37,11 +36,27 @@ def create_app(env_name):
     app.register_blueprint(check_blueprint, url_prefix='/api/v1/checks')
     app.register_blueprint(patient_info_blueprint, url_prefix='/api/v1/patient_infos')
 
+    app.config['SECRET'] = 'my secret key'
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['MQTT_BROKER_URL'] = 'localhost'
+    app.config['MQTT_BROKER_PORT'] = 1883
+    app.config['MQTT_CLIENT_ID'] = 'flask_mqtt'
+    app.config['MQTT_USERNAME'] = ''
+    app.config['MQTT_PASSWORD'] = ''
+    app.config['MQTT_KEEPALIVE'] = 5
+    app.config['MQTT_TLS_ENABLED'] = False
+    app.config['MQTT_LAST_WILL_TOPIC'] = 'home/lastwill'
+    app.config['MQTT_LAST_WILL_MESSAGE'] = 'bye'
+    app.config['MQTT_LAST_WILL_QOS'] = 2
+
+    # Parameters for SSL enabled
+    # app.config['MQTT_BROKER_PORT'] = 8883
+    # app.config['MQTT_TLS_ENABLED'] = True
+    # app.config['MQTT_TLS_INSECURE'] = True
+    # app.config['MQTT_TLS_CA_CERTS'] = 'ca.crt'
+
     @app.route('/', methods=['GET'])
     def index():
-        """
-        example endpoint
-        """
-        return 'Welcome to Fitbox2Go API!'
+        return render_template('index.html')
 
     return app
